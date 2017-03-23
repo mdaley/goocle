a81a4cdbb64c9bd6069fcb82bf12e26e467b4b6c
Matt Daley <m6daley@gmail.com>
2017-03-23 18:11:10 +0000

    Kebab casing doesn't separate numbers from preceding text, i.e. BlahBlah1Blah -> blah-blah1-blah. Fixed broken tests. Some additional tidying.

--- src/goocle/core.clj
+++ src/goocle/core.clj
@@ -4,7 +4,7 @@
              [builder :as b]]
             [clojure
                 [set :refer [map-invert]]
-             [string :refer [ends-with?]]]
+             [string :refer [ends-with? replace]]]
             [camel-snake-kebab.core :refer [->kebab-case]
              ])
   (:import [java.lang.reflect Modifier]))
@@ -18,17 +18,26 @@
     (create-ns ns-sym)
     (intern ns-sym name-sym (eval (read-string ftn)))))
 
+(defn- fixup-dash-number
+  "Fixup any instances of -[0-9] to be [0-9] so that numbers aren't separated from their preceding text (or numbers)."
+  [s]
+  (replace s #"\-([0-9])" "$1"))
+
 (defn- ftn-name
   [clazz]
+  (-> (.getSimpleName clazz)
+      ->kebab-case
+      fixup-dash-number)
   (->kebab-case (.getSimpleName clazz)))
 
 (defn- local-ns
   [clazz]
   (-> (.getPackage clazz)
-      (.getName)
+      .getName
       (clojure.string/replace #"com.google.cloud." "")
       (clojure.string/replace #"\." "-")
-      (->kebab-case)
+      ->kebab-case
+      fixup-dash-number
       ((partial str "goocle."))))
 
 (defn intern-all-builders
@@ -36,5 +45,4 @@
   (let [r (r/get-reflector-for-namespace "com.google.cloud")
         builder-classes (r/get-classes-with-method r "newBuilder")
         fns (map (fn [bc] {:ns (local-ns bc) :name (str (ftn-name bc) "-builder") :ftn (b/build-fn (.getName bc) "newBuilder")}) builder-classes)]
-    (map #(do (println "Interning " %)
-              (intern-fn %)) fns)))
+    (map intern-fn fns)))
--- test/goocle/builder_test.clj
+++ test/goocle/builder_test.clj
@@ -16,24 +16,24 @@
   (testing "Non-overloaded static function with single argument definition built correctly"
     (test-function-build {:methods-args '(({:name "name"
                                             :type "the.Type"}))
-                          :expected "(fn [{:keys [name]}] (cond (and name (instance? the.Type name)) (the.Class/theFunction name) :else (throw IllegalArgumentException.)))"}))
+                          :expected "(fn [{:keys [name]}] (cond (and name (instance? the.Type name)) (the.Class/theFunction name) :else (throw (IllegalArgumentException. \"Please use valid combination of arguments from the set 'name'\"))))"}))
 
   (testing "Function names are not changed"
     (test-function-build {:methods-args '(({:name "name"
                                             :type "the.namespace.and.TypeLikeThis"}))
-                          :expected "(fn [{:keys [name]}] (cond (and name (instance? the.namespace.and.TypeLikeThis name)) (the.Class/theFunction name) :else (throw IllegalArgumentException.)))"}))
+                          :expected "(fn [{:keys [name]}] (cond (and name (instance? the.namespace.and.TypeLikeThis name)) (the.Class/theFunction name) :else (throw (IllegalArgumentException. \"Please use valid combination of arguments from the set 'name'\"))))"}))
 
   (testing "Argument names are transformed to kebab case"
     (test-function-build {:methods-args '(({:name "nameLikeThis"
                                             :type "the.Type"}))
-                          :expected "(fn [{:keys [name-like-this]}] (cond (and name-like-this (instance? the.Type name-like-this)) (the.Class/theFunction name-like-this) :else (throw IllegalArgumentException.)))"}))
+                          :expected "(fn [{:keys [name-like-this]}] (cond (and name-like-this (instance? the.Type name-like-this)) (the.Class/theFunction name-like-this) :else (throw (IllegalArgumentException. \"Please use valid combination of arguments from the set 'name-like-this'\"))))"}))
 
   (testing "Non-overloaded multi-argument static function definition built correctly"
     (test-function-build {:methods-args '(({:name "name"
                                             :type "the.Type"}
                                            {:name "other-name"
                                             :type "the.OtherType"}))
-                          :expected "(fn [{:keys [name other-name]}] (cond (and name other-name (instance? the.Type name) (instance? the.OtherType other-name)) (the.Class/theFunction name other-name) :else (throw IllegalArgumentException.)))"}))
+                          :expected "(fn [{:keys [name other-name]}] (cond (and name other-name (instance? the.Type name) (instance? the.OtherType other-name)) (the.Class/theFunction name other-name) :else (throw (IllegalArgumentException. \"Please use valid combination of arguments from the set 'name other-name'\"))))"}))
 
   (testing "Overloaded static function with range of arguments definition built correctly"
     (test-function-build {:methods-args '(({:name "name"
@@ -42,7 +42,7 @@
                                             :type "the.Other"})
                                           ({:name "name"
                                             :type "the.Type"}))
-                          :expected "(fn [{:keys [name other]}] (cond (and name other (instance? the.Type name) (instance? the.Other other)) (the.Class/theFunction name other) (and name (instance? the.Type name)) (the.Class/theFunction name) :else (throw IllegalArgumentException.)))"}))
+                          :expected "(fn [{:keys [name other]}] (cond (and name other (instance? the.Type name) (instance? the.Other other)) (the.Class/theFunction name other) (and name (instance? the.Type name)) (the.Class/theFunction name) :else (throw (IllegalArgumentException. \"Please use valid combination of arguments from the set 'name other'\"))))"}))
 
   (testing "Overloaded range of arguments static function definition built correctly when there is a zero args overload"
     (test-function-build {:methods-args '(({:name "name"
