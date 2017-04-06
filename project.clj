f0832117693d6cc51fe08ed28c5ecb028e039d6f
Matt Daley <m6daley@gmail.com>
2017-04-06 20:19:55 +0100

    Targeting google cloud jara 0.7.0.

--- project.clj
+++ project.clj
@@ -5,23 +5,11 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [camel-snake-kebab "0.4.0" :exclusions [org.clojure/clojure]]
-                 [com.google.cloud/google-cloud "0.11.1-WithParameters-alpha"
+                 [com.google.cloud/google-cloud "0.7.0-WithParameters"
                   :exclusions [io.grpc/grpc-core
-                               io.netty/netty-codec-http2
-                               com.google.http-client/google-http-client-jackson2
-                               com.google.http-client/google-http-client
-                               com.google.api.grpc/grpc-google-common-protos
-                               com.google.protobuf/protobuf-java-util
-                               com.google.protobuf/protobuf-java
-                               com.google.api-client/google-api-client]]
-                 [com.google.api.grpc/grpc-google-cloud-language-v1 "0.1.5"
-                  :exclusions [com.google.auto.value/auto-value
-                               io.grpc/grpc-protobuf-lite
-                               io.grpc/grpc-core
                                io.netty/netty-codec-http2]]
-                 [io.grpc/grpc-core "1.2.0"]
+                 [io.grpc/grpc-core "1.0.3"]
                  [io.netty/netty-codec-http2 "4.1.9.Final"]
-                 [com.google.api-client/google-api-client "1.22.0"]
                  [org.reflections/reflections "0.9.11"
                   :exclusions [com.google.guava/guava]]
                  [javax.servlet/servlet-api "2.5"]])
