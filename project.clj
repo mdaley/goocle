bd42ba288fd6d701608ce97f06176ffbf9086ae8
Matt Daley <m6daley@gmail.com>
2017-04-12 16:27:14 +0100

    Added clojure/tools.logging.

--- project.clj
+++ project.clj
@@ -4,6 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
+                 [org.clojure/tools.logging "0.3.1"]
                  [camel-snake-kebab "0.4.0" :exclusions [org.clojure/clojure]]
                  [com.google.cloud/google-cloud "0.7.0-WithParameters"
                   :exclusions [io.grpc/grpc-core
